d180510ae40f63e9fc56fa6b947ab3a80cb9703f
prachi jatav <prachijatav72@gmail.com>
2025-03-25 18:38:18 +0530

    fourth commit

--- src/FirstDiv.jsx
+++ src/FirstDiv.jsx
@@ -8,30 +8,14 @@ function FirstDiv() {
   return (
     <div className="FDiv">
       <div className="firstDIv">
-        <Card  className="cardBody">
+        <Card className="cardBody">
           <Card.Body>
-            <Card.Title><FiMenu color="rgb(53, 147, 248)" size={43} /> </Card.Title>
-            <Card.Subtitle className="mb-2  CardSubTxt">
-              
-Can’t easily produce an accurate master lease list?
+            <Card.Title><FiMenu color="rgb(53, 147, 248)" size={43} /></Card.Title>
+            <Card.Subtitle className="mb-2 CardSubTxt">
+              Can’t easily produce an accurate master lease list?
             </Card.Subtitle>
             <Card.Text className="cardTxt">
-              As your portfolio begins to grow, it can become difficult to
-              maintain an accurate and detailed list of each and every location.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </div>
-      <div className="firstDIv">
-        <Card  className="cardBody">
-          <Card.Body>
-            <Card.Title><TbFileSpreadsheet color="rgb(53, 147, 248)" size={43}  /></Card.Title>
-            <Card.Subtitle className="mb-2 CardSubTxt">        
-Spreadsheets becoming a hassle to maintain?
-            </Card.Subtitle>
-            <Card.Text className="cardTxt">
-              Spreadsheets are great for viewing data, not managing data. It’s
-              easy to make mistakes and hard to keep things organized.
+              As your portfolio begins to grow, it can become difficult to maintain an accurate and detailed list of each and every location.
             </Card.Text>
           </Card.Body>
         </Card>
@@ -39,13 +23,25 @@ Spreadsheets becoming a hassle to maintain?
       <div className="firstDIv">
         <Card className="cardBody">
           <Card.Body>
-            <Card.Title><HiOutlineClipboardDocumentList color="rgb(53, 147, 248)" size={43}  />
-            </Card.Title>
-            <Card.Subtitle className="mb-2  CardSubTxt">
-            Tired of sifting through documents to find key information?
+            <Card.Title><TbFileSpreadsheet color="rgb(53, 147, 248)" size={43} /></Card.Title>
+            <Card.Subtitle className="mb-2 CardSubTxt">        
+              Spreadsheets becoming a hassle to maintain?
             </Card.Subtitle>
             <Card.Text className="cardTxt">
-            Going page by page through a lease document when you need a quick answer is frustrating and time consuming. 
+              Spreadsheets are great for viewing data, not managing data. It’s easy to make mistakes and hard to keep things organized.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="firstDIv">
+        <Card className="cardBody">
+          <Card.Body>
+            <Card.Title><HiOutlineClipboardDocumentList color="rgb(53, 147, 248)" size={43} /></Card.Title>
+            <Card.Subtitle className="mb-2 CardSubTxt">
+              Tired of sifting through documents to find key information?
+            </Card.Subtitle>
+            <Card.Text className="cardTxt">
+              Going page by page through a lease document when you need a quick answer is frustrating and time consuming. 
             </Card.Text>
           </Card.Body>
         </Card>
--- src/SavenPage.jsx
+++ src/SavenPage.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import listimg from './images/list.png'
 const SavenPage = () => {
   return (
-    <div className='sevenpage' style={{ display: 'flex', alignItems: 'center', height:"100vh" ,justifyContent:'center', gap:'30px',margin:'20px'}}>
-        <div className="left">
-        <img src={listimg} width={950} />
-
-        </div>
-        <div className="right" style={{textAlign:'center', margin:'auto'}}>
-            <p style={{fontSize:25}}>...or catch a glimpse of all your leases with the Master Lease List</p>
-        </div>
+    <>
+   
+    <div className='sevenpage' >
+      <div className="left">
+        <img src={listimg}  />
+      </div>
+      <div className="right" style={{ textAlign: 'center', margin: 'auto' }}>
+        <p style={{ fontSize: '25px' }}>...or catch a glimpse of all your leases with the Master Lease List</p>
+      </div>
     </div>
+    </>
   )
 }
 
